585fc3990587f9fdef84f5ebc74e2339e1271ae1
EPVISHNU <vishnuep8089@gmail.com>
2024-10-29 07:13:04 +0530

    added a route for root url

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ const wserver = express()
 
 wserver.use(express.json())
 wserver.use (cors())
+wserver.get("/", (req, res) => {
+    res.send("Weather API is up and running!");
+});
+
 wserver.use('/api/weather',routes)
 
 
